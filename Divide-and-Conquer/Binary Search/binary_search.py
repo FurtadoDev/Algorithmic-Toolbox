8c52fc36a029686418661253353c494271b99190
FurtadoDev <vrfurtado@mun.ca>
2021-05-22 08:04:48 -0230

    Updated solutions for Binary Search

--- Divide-and-Conquer/Binary Search/binary_search.py
+++ Divide-and-Conquer/Binary Search/binary_search.py
@@ -1,4 +1,7 @@
 # python3
+import math
+
+data_set = []
 
 
 def linear_search(keys, query):
@@ -9,11 +12,29 @@ def linear_search(keys, query):
     return -1
 
 
+def recurse(l_idx, r_idx, check_for):
+    global data_set
+    if l_idx == r_idx:
+        if data_set[l_idx] == check_for:
+            return l_idx
+        else:
+            return -1
+    else:
+        mid_idx = math.floor((l_idx + r_idx)/2)
+        if check_for <= data_set[mid_idx]:
+            return recurse(l_idx, mid_idx, check_for)
+        else:
+            return recurse(mid_idx+1, r_idx, check_for)
+
+
 def binary_search(keys, query):
     assert all(keys[i] < keys[i + 1] for i in range(len(keys) - 1))
     assert 1 <= len(keys) <= 3 * 10 ** 4
-
-    type here
+    global data_set
+    data_set = keys
+    data_set_size = len(keys)
+    index = recurse(0, data_set_size-1, query)
+    return index
 
 
 if __name__ == '__main__':
