bb937483a147b34e88f96fa8e685b8c7a4e9306a
FurtadoDev <vrfurtado@mun.ca>
2021-06-10 21:25:45 -0230

    Added Solution for Improving Quick Sort

--- Divide-and-Conquer/Improving QuickSort/quicksort.py
+++ Divide-and-Conquer/Improving QuickSort/quicksort.py
@@ -2,18 +2,36 @@
 
 from random import randint
 
+global_array = []
+
 
 def partition3(array, left, right):
-    type here
+    # condition when left and right are very close
+    i = left + 1
+    for j in range(left + 1, right + 1):
+        if array[j] < array[left]:
+            temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+            i = i + 1
+    temp = array[i-1]
+    array[i-1] = array[left]
+    array[left] = temp
+    # return the index around which the array is partitioned
+    return i-1
 
 
 def randomized_quick_sort(array, left, right):
     if left >= right:
         return
     k = randint(left, right)
+    # make pivot first
     array[left], array[k] = array[k], array[left]
-    make a call to partition3 and then two recursive calls 
-to randomized_quick_sort
+    # make a call to partition3 and then two recursive calls
+    # to randomized_quick_sort
+    partition_idx = partition3(array, left, right)
+    randomized_quick_sort(array, left, partition_idx - 1)
+    randomized_quick_sort(array, partition_idx + 1, right)
 
 
 if __name__ == '__main__':
--- Divide-and-Conquer/Improving QuickSort/quicksort_unit_tests.py
+++ Divide-and-Conquer/Improving QuickSort/quicksort_unit_tests.py
@@ -8,7 +8,7 @@ class TestQuickSort(unittest.TestCase):
         for array in [
             ([1, 2, 3]),
             ([3, 2, 1]),
-            type here
+            ([5, 4, 3, 8, 1, 2, 3, 0, 1, 15, 20, 50, 33, 11, 10, 9, 7])
         ]:
             sorted_array = sorted(list(array))
             randomized_quick_sort(array, 0, len(array) - 1)
